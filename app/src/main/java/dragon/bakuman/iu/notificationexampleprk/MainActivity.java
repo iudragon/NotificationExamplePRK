eb0a31261aae7978c2b05814d254ad6dad48c248
TeamLewok <42891947+TeamLewok@users.noreply.github.com>
2018-12-22 16:13:41 +0530

    Initial commit

---
+++ app/src/main/java/dragon/bakuman/iu/notificationexampleprk/MainActivity.java
@@ -0,0 +1,57 @@
+package dragon.bakuman.iu.notificationexampleprk;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String CHANNEL_ID = "personal_notifs";
+    private final int NOTIFICATION_ID = 001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayNotif(View view) {
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_chat);
+        builder.setContentTitle("SImple Notification");
+        builder.setContentText("This si a smiple notifs");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+
+    }
+
+     //for Android version 8.0 + we need more methods!
+    private void createNotificationChannel(){
+
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+
+            CharSequence name = "Personal Notifications";
+            String description = "Include all the personal notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+}
